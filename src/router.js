a0d0b882cd84886eee3ba80c2fd5492f35b839d5
David Barragán Merino <david.barragan@kaleidos.net>
2016-12-20 11:56:17 +0100

    Fix a typo

--- src/router.js
+++ src/router.js
@@ -2,10 +2,10 @@ import VueRouter from 'vue-router'
 import miComponent from './components/micomponent/micomponent'
 
 const router = new VueRouter({
-  mode: 'history',q
-  routes: [
-    { path: '/test', component: miComponent }
-  ]
+    mode: 'history',
+    routes: [
+        { path: '/test', component: miComponent }
+    ]
 })
 
 export { VueRouter, router }
