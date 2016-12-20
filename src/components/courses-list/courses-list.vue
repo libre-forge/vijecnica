5182c6682a861af99e0af28a3887b6e184a796ab
Xaviju <xaviju@gmail.com>
2016-12-20 16:12:33 +0100

    Mock component data

--- src/components/courses-list/courses-list.vue
+++ src/components/courses-list/courses-list.vue
@@ -1,11 +1,38 @@
 
+
+
 <template src='./courses-list.html'></template>
+
+
 
 <style scoped src='./courses-list.css'></style>
 
 <script>
 export default {
     name: 'courses-list',
+    data: function () {
+        return {
+            courses: [{
+                name: 'GraphSQL',
+                description: 'Learn GrapSQL',
+                members: 5,
+                members_limit: 10,
+                status: 'active'
+            }, {
+                name: 'Browser Internal',
+                description: 'Use the platform',
+                members: 5,
+                members_limit: 10,
+                status: 'active'
+            }, {
+                name: 'Cocina con Bertín',
+                description: 'Bertin y Arévalo alegrarán tu cocina',
+                members: 5,
+                members_limit: 10,
+                status: 'active'
+            }]
+        }
+    },
     props: {
         subject: {
             type: String,
@@ -14,7 +41,7 @@ export default {
     },
     watch: {
         subject: function (val) {
-            console.log(val) // API CALL
+            console.log(val) // API CALL and overwrite courses
         }
     }
 }
