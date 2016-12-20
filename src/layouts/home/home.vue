8c10bd34e7589f868f0cb709b872f9adc11cbef5
Xaviju <xaviju@gmail.com>
2016-12-20 13:20:03 +0100

    Apply home layout and component

---
+++ src/layouts/home/home.vue
@@ -0,0 +1,26 @@
+
+<template src='./home.html'></template>
+
+<style scoped src='./home.css'></style>
+
+<script>
+import Searchbox from '../../components/searchbox/searchbox'
+
+export default {
+    name: 'home',
+    data () {
+        return {
+            displayCourses: false,
+            pitch: 'Do not invest time in learning, burn it.'
+        }
+    },
+    components: {
+        Searchbox
+    },
+    methods: {
+        isSearching: function () {
+            this.displayCourses = true
+        }
+    }
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,12 @@
 import VueRouter from 'vue-router'
 import miComponent from './components/micomponent/micomponent'
-
+import home from './layouts/home/home'
 import AddCourse from './layouts/add-course/add-course'
 
 const router = new VueRouter({
     mode: 'history',
     routes: [
+        { path: '/', component: home },
         { path: '/test', component: miComponent },
         { path: '/course/add', component: AddCourse }
     ]
