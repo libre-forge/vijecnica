19bee4294320fb0b4fe22705b73f39c44836d849
Xaviju <xaviju@gmail.com>
2016-12-21 20:23:35 +0100

    Login Page

---
+++ src/layouts/login/login.vue
@@ -0,0 +1,14 @@
+<template src='./login.html'></template>
+
+<style scoped src='./login.css'></style>
+
+<script>
+import loginForm from '../../components/login-form/login-form'
+
+export default {
+    name: 'login',
+    components: {
+        loginForm
+    }
+}
+</script>
