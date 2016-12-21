d71c58f6c6fb6fbcc37059d66c06d4e485c53579
David Barragán Merino <david.barragan@kaleidos.net>
2016-12-21 21:04:13 +0100

    Create subject detail layout (and dummy subjects-list)

---
+++ src/layouts/subject/subject.vue
@@ -0,0 +1,27 @@
+<template src='./subject.html'></template>
+
+<style scoped src='./subject.css'></style>
+
+<script>
+import subjectDetail from '../../components/subject-detail/subject-detail'
+import wall from '../../components/wall/wall'
+
+export default {
+    name: 'subject',
+    data: function () {
+        return {
+            user: {
+                id: 1,
+                username: 'Leslie Burton',
+                avatar: 'https://randomuser.me/api/portraits/women/35.jpg',
+                email: 'leslie.burton45@example.com'
+            }
+        }
+    },
+    components: {
+        subjectDetail,
+        // resourcesList,
+        wall
+    }
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -3,13 +3,15 @@ import VueRouter from 'vue-router'
 import home from './layouts/home/home'
 import AddCourse from './layouts/add-course/add-course'
 import course from './layouts/course/course'
+import subject from './layouts/subject/subject'
 
 const router = new VueRouter({
     mode: 'history',
     routes: [
         { path: '/', name: 'home', component: home, isDefault: true },
         { path: '/course/add', name: 'add-course', component: AddCourse },
-        { path: '/course/:id', name: 'course', component: course }
+        { path: '/course/:id', name: 'course', component: course },
+        { path: '/course/:courseId/subject/:subjectId', name: 'subject', component: subject }
     ]
 })
 
