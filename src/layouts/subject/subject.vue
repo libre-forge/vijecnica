5e895480bd2698ff27bf4a2bd90770037b800130
David Barragán Merino <david.barragan@kaleidos.net>
2016-12-22 16:11:33 +0100

    Get subject from the api

--- src/layouts/subject/subject.vue
+++ src/layouts/subject/subject.vue
@@ -7,18 +7,47 @@ import Navigation from '../../components/nav/nav'
 import subjectDetail from '../../components/subject-detail/subject-detail'
 import wall from '../../components/wall/wall'
 import resources from '../../components/resources/resources'
+import gql from 'graphql-tag'
 
+const subjectQuery = gql`
+    query Subject($id: String!) {
+        subject(id: $id) {
+            id
+            title
+            description
+            course {
+                id
+                title
+            }
+            resource_count
+            resources {
+                id
+                title
+                description
+                mime
+                uri
+                subject
+                created_at
+                created_by
+            }
+            order
+            finished_at
+        }
+    }
+`
 export default {
     name: 'subject',
     data: function () {
         return {
+            courseId: this.$route.params.courseId,
+            subjectId: this.$route.params.subjectId,
+            subject: {},
             user: {
                 id: 1,
                 username: 'Leslie Burton',
                 avatar: 'https://randomuser.me/api/portraits/women/35.jpg',
                 email: 'leslie.burton45@example.com'
-            },
-            subjectId: this.$route.params.subjectId
+            }
         }
     },
     components: {
@@ -26,6 +55,14 @@ export default {
         subjectDetail,
         resources,
         wall
+    },
+    apollo: {
+        subject: {
+            query: subjectQuery,
+            variables () {
+                return { id: this.subjectId }
+            }
+        }
     }
 }
 </script>
